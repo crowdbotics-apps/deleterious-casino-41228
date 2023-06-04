37e2e1f546c2799206e325d9fbdb7ffb6e3abd66
Crowdbotics <team@crowdbotics.com>
2023-06-04 05:59:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "deleterious-casino-41228",
+  url: "https://deleterious-casino-41228.botics.co",
+  api: "https://deleterious-casino-41228.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
